e390abe4eb01ebdfb696ee6774db83389b92337f
Iva Karadjova <ivakaradjova@yahoo.co.uk>
2023-12-04 21:36:55 +0000

    Add functionality links to close mobile menu on link click. Add forEach() to mobLink.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let btn = document.querySelector('.hamburger-icon')
 let hMenu = document.querySelector(".hamburger-menu")
 
-
 btn.addEventListener('click', () => {
  console.log('click');
  hMenu.style.transform = 'translateX(0%)' 
  hMenu.style.transition = ".9s" 
 })
+
+
+let  mobLink = document.querySelectorAll('.mobile-link')
+console.log(mobLink);
+mobLink.forEach((link) => link.addEventListener('click' , () => {
+ console.log('link');
+  hMenu.style.transform = 'translateX(100%)' 
+  hMenu.style.transition = ".9s" 
+}))
+
